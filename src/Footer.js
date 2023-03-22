d2f00435246d7420e09ee65d6f6d7b08354bb7b6
Irene Smolchenko <smolchenko@gmail.com>
2023-03-22 09:59:46 +0000

    "Initial commit"

---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <span>Reenie Smooch ©2023</span>
+    </footer>
+  );
+};
+
+export default Footer;
